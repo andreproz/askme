84e1ba51666e936405389dd3d209ee5d9e6ad1d8
Andrew Prozorov <andreproz@mail.ru>
2023-01-29 04:33:44 +0300

    Added authors

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+  def question_author(question)
+    if question.author
+      link_to display_nickname(question.author), user_path(question.author)
+    else
+      content_tag :b, 'Неизвестный'
+    end
+  end
 end
