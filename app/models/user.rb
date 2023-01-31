bef76c0797321db407624d8cb5be95e975f4089a
Andrew Prozorov <andreproz@mail.ru>
2023-01-31 13:07:03 +0300

    Add unique index on email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   
   has_secure_password
   
-  has_many :questions, dependent: :delete_all
+  has_many :questions, dependent: :destroy
   
   before_validation :downcase_user_params
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_29_010852) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_31_095541) do
   create_table "questions", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
@@ -31,7 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_29_010852) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "head_color", default: "#370617"
-    t.index ["email"], name: "index_users_on_email"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["nickname"], name: "index_users_on_nickname", unique: true
   end
 
